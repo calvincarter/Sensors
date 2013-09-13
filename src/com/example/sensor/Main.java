9380d54e8f45677212f6bcd788fbc8c0e6756407
Calvin <Calvin@192.168.43.38>
2013-09-13 17:24:06 -0500

    V 1.2

--- src/com/example/sensor/Main.java
+++ src/com/example/sensor/Main.java
@@ -1,5 +1,8 @@
 package com.example.sensor;
 
+
+import java.util.List;
+
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -8,11 +11,26 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.widget.TextView;
 
-public class Main extends Activity implements SensorEventListener{
+public class Main extends Activity implements SensorEventListener {
 
 	Sensor accelerometer;
 	SensorManager sm;
 	TextView acceleration;
+	
+	TextView gyroscope;
+	
+	Sensor gyro;
+	
+	static float accel_X;
+	static float accel_Y;
+	static float accel_Z;
+	
+	static float gyro_X;
+	static float gyro_Y;
+	
+	IMU go;
+	
+	List<Sensor> sensorList;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +38,20 @@ public class Main extends Activity implements SensorEventListener{
 		setContentView(R.layout.main);
 		
 		sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+				
 		accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		gyro = sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 		
 		sm.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+		sm.registerListener(this, gyro, SensorManager.SENSOR_DELAY_NORMAL);
 		
 		acceleration = (TextView) findViewById(R.id.acceleration);
+		gyroscope = (TextView) findViewById(R.id.gyroscope);
+		
+		
+		
+		go = new IMU();
+		go.setup();
 	}
 
 	@Override
@@ -35,11 +62,31 @@ public class Main extends Activity implements SensorEventListener{
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		// TODO Auto-generated method stub
 		
-		acceleration.setText("X: "+ event.values[0] +
-				"\nY: " + event.values[1] +
-				"\nZ: " + event.values[2]); 
+        Sensor sensor = event.sensor;
+        
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+        	accel_X = event.values[0];
+    		accel_Y = event.values[1];
+    		accel_Z = event.values[2];
+    		
+    		acceleration.setText("Accel - X: "+ accel_X +
+    				"\nY: " + accel_Y +
+    				"\nZ: " + accel_Z);
+        }
+        else if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+        	
+        	gyro_X = event.values[0];
+    		gyro_Y = event.values[1];
+    		
+    		gyroscope.setText("Gyro - X: "+ gyro_X +
+    				"\nY: " + gyro_Y +
+    				"\nZ: " + accel_Z);
+            
+        }
+        
+        //go.getEstimatedInclination();
+        
 	}
 
 }
